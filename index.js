9067373f3f49db3710d258432508a4c2e293b1ba
Farkhod Kodirov <farxod94@gmail.com>
2023-08-17 02:54:10 +0500

    feat: add get tags

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ server.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
 
-const tags = new Set();
 app.post("/messages", async (req, res) => {
   const { text, tags: messageTags } = req.body;
   try {
@@ -36,6 +35,39 @@ app.post("/messages", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Not saved!" });
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  try {
+    const messages = await Message.findAll();
+    res.json(messages);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching messages." });
+  }
+});
+
+app.get("/tags", async (req, res) => {
+  try {
+    const messages = await Message.findAll({
+      attributes: ["tags"],
+    });
+    const tags = messages.map((message) => message.tags);
+    const allTags = [];
+
+    tags.forEach((item) => {
+      const tagsInItem = item.match(/#[a-z]+/gi);
+      if (tagsInItem) {
+        allTags.push(...tagsInItem);
+      }
+    });
+    res.json(allTags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching tags." });
   }
 });
 
