9e604d2d2cd3d61396c597b73aecf97399b93d9e
Farkhod Kodirov <farxod94@gmail.com>
2023-08-18 02:20:46 +0500

    feat: add dotenv

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("chat", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 module.exports = sequelize;
